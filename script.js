eafb7c31dcadf73793e953d15ecc087ce9c5a753
Dr. Cat <47220664+catsmells@users.noreply.github.com>
2024-08-25 10:13:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,31 @@ function makeDraggable(element) {
     }
 }
 
+// Function to make windows resizable
+function makeResizable(element) {
+    const handle = element.querySelector('.resize-handle');
+    let startX, startY, startWidth, startHeight;
+
+    handle.addEventListener('mousedown', function(e) {
+        startX = e.clientX;
+        startY = e.clientY;
+        startWidth = parseInt(document.defaultView.getComputedStyle(element).width, 10);
+        startHeight = parseInt(document.defaultView.getComputedStyle(element).height, 10);
+        document.addEventListener('mousemove', resizeWindow);
+        document.addEventListener('mouseup', stopResizingWindow);
+    });
+
+    function resizeWindow(e) {
+        element.style.width = `${startWidth + (e.clientX - startX)}px`;
+        element.style.height = `${startHeight + (e.clientY - startY)}px`;
+    }
+
+    function stopResizingWindow() {
+        document.removeEventListener('mousemove', resizeWindow);
+        document.removeEventListener('mouseup', stopResizingWindow);
+    }
+}
+
 // Function to close windows
 function setupCloseButtons() {
     document.querySelectorAll('.close-btn').forEach(button => {
@@ -49,14 +74,19 @@ function createNewWindow() {
         <div class="content">
             <p>This is a new RetrofleX window.</p>
         </div>
+        <div class="resize-handle"></div>
     `;
     document.body.appendChild(newWindow);
     makeDraggable(newWindow);
+    makeResizable(newWindow);
     setupCloseButtons();
 }
 
 // Initialize existing windows
-document.querySelectorAll('.window').forEach(makeDraggable);
+document.querySelectorAll('.window').forEach(win => {
+    makeDraggable(win);
+    makeResizable(win);
+});
 setupCloseButtons();
 
 // Set up the "New Window" button
